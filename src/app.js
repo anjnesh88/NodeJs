019493181391cf5bda8449dc60c7f0be34ce52f4
ANJNESH\Anjnesh Vasudeva <anvasudeva@deloitte.com.au>
2025-06-25 18:03:20 +0530

    Custom parms

--- src/app.js
+++ src/app.js
@@ -2,23 +2,28 @@ const express = require('express');
 const app = express();
 
 //This will match all the HTTP method API call to /test
-app.use('/test',(req,res)=>{
-    res.send('Hello from the test')
-});
+// app.use('/test',(req,res)=>{
+//     res.send('Hello from the test')
+// });
 
-//This will only handle get call to /user
-app.get("/user", (req, res)=>{
+// //This will only handle get call to /user
+// app.get("/user", (req, res)=>{
+//     res.send({firstName: "Anjnesh", lastName: "Vasudeva"})
+// })
+
+// //This will only handle post call to /user
+// app.post("/user", (req, res)=>{
+//     console.log("Save data to database");
+//     res.send("Data saved successfully");
+// })
+
+// app.delete("/user", (req, res)=>{
+//     res.send("Data deleted successfully");
+// })
+
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
     res.send({firstName: "Anjnesh", lastName: "Vasudeva"})
-})
-
-//This will only handle post call to /user
-app.post("/user", (req, res)=>{
-    console.log("Save data to database");
-    res.send("Data saved successfully");
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("Data deleted successfully");
 })
 
 app.listen(3000, ()=>{
