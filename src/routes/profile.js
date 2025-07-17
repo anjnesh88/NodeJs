8c09629469bd9bd1c322e44141dacf71f491c27b
ANJNESH\Anjnesh Vasudeva <anvasudeva@deloitte.com.au>
2025-07-17 12:41:16 +0530

    Reset password API created

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,9 @@ const { userAuth } = require('../middlewares/auth');
 const { validateEditProfileData } = require('../utils/validation');
 const cookieParser = require('cookie-parser');
 const profileRouter = express.Router();
+const bcrypt = require('bcrypt');
+const { User } = require('../models/user');
+var validator = require('validator');
 
 profileRouter.use(express.json());
 profileRouter.use(cookieParser());
@@ -29,6 +32,28 @@ profileRouter.patch("/profile/edit", userAuth, async(req, res)=>{
             message: `${loggedInUser.firstName}, your profile updated succesfully`,
             data: loggedInUser
         });
+    }catch(err){
+        res.status(400).send("Error: " + err.message)
+    }
+});
+
+profileRouter.patch("/profile/password", userAuth, async(req, res)=>{
+    try{
+        const loggedInUser = req.user;
+        if(req.body.oldPassword){
+            const isPasswordValid = await bcrypt.compare(req.body.oldPassword, loggedInUser.password);
+            if(isPasswordValid){
+                if(!validator.isStrongPassword(req.body.newPassword)){
+                    throw new Error("Enter a strong password: " + req.body.newPassword);
+                }
+                const passwordHash = await bcrypt.hash(req.body.newPassword, 10);
+                const user = await User.findByIdAndUpdate(loggedInUser._id, {password: passwordHash});
+                user.save();
+                res.send("Password updated successfully!");
+            }else{
+                throw new Error("Password is not correct");
+            }
+        }
     }catch(err){
         res.status(400).send("Error: " + err.message)
     }
