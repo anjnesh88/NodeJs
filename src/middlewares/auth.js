91bab338dc03bb4f38526faddf6333ba6e97c87b
ANJNESH\Anjnesh Vasudeva <anvasudeva@deloitte.com.au>
2025-08-05 16:26:39 +0530

    auth js changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,29 @@
-const jwt = require('jsonwebtoken');
-const { User } = require('../models/user');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const userAuth = async (req, res, next)=>{
-    try{
-        const { token } = req.cookies;
-        if(!token){
-            throw new Error("Token is not valid");
-        }
-        const decodedObj = await jwt.verify(token, "Vasu@123");
-        const {_id} = decodedObj;
-        const user = await User.findById(_id);
-        if(!user){
-            throw new Error("User not found");
-        }
-        req.user = user;
-        next();
-    }catch(err){
-        res.status(400).send("Erro: "+ err.message);
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please Login!");
     }
-}
-module.exports = {userAuth}
+
+    const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+};
+
+module.exports = {
+  userAuth,
+};
